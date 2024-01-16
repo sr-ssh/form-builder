76f69bab3cad62387eaf9e0db78197a8f949f3f1
sr-ssh <r.sadatshokouhi@gmail.com>
2024-01-16 10:38:13 +0330

    feat: :sparkles: add a default animation to partial pages

--- src/core/components/containers/PartialTabContainer.tsx
+++ src/core/components/containers/PartialTabContainer.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ViewComponent } from "../ViewComponent";
 import ViewContextProvider from "../../context/ViewContextProvider";
 import { useViewManage } from "../../hooks/useViewManage";
-import {
-  closeTabAnimationConfig,
-  onLeaveContainerConfig,
-  openTabContainerConfig,
-  slideIn,
-} from "../../utils/viewAnimations";
+import { ViewEvent } from "../../@types/view";
+import { bezier } from "../../utils/bezier";
 
 const PartialTabContainer = ({
   containerName,
@@ -16,82 +12,127 @@ const PartialTabContainer = ({
   containerName: string;
   className: string;
 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const openConfig: ViewEvent = {
+    duration: 400,
+    start(newViewEl, prevViewEl) {
+      const containerEl = containerRef.current;
+      const newStyle = newViewEl.ref.style;
+      let prevStyle = prevViewEl?.ref.style;
+      if (containerEl && viewsInfo.length <= 1) {
+        console.log(containerEl.offsetWidth);
+        containerEl.style.marginLeft = `${-containerEl.offsetWidth * 1.5}px`;
+        console.log(containerEl.style.marginLeft);
+        containerEl.style.display = "block";
+      } else {
+        if (prevStyle) {
+          prevStyle.position = "absolute";
+          prevStyle.top = "0";
+          prevStyle.width = "100%";
+          prevStyle.zIndex = "2";
+        }
+        if (newStyle) {
+          newStyle.display = "block";
+          newStyle.opacity = "1";
+          newStyle.zIndex = "2";
+        }
+      }
+    },
+    animate(t, newViewEl, prevViewEl) {
+      const containerEl = containerRef.current;
+      const newStyle = newViewEl.ref.style;
+      const prevStyle = prevViewEl?.ref.style;
+      const p = bezier(0.25, 1, 0.5, 1)(t);
+      if (containerEl && viewsInfo.length <= 1) {
+        containerEl.style.marginLeft = `${
+          (p - 1) * containerEl.offsetWidth * 1.5
+        }px`;
+        if (prevStyle) {
+          prevStyle.transform = `translateX(-${(p - 1) * 0.1 * 100}%)`;
+        }
+      } else {
+        if (prevStyle) {
+          console.log("containerEl.style.marginLeft");
+          prevStyle.transform = `translateX(${p * 100}%)`;
+        }
+        if (newStyle) {
+          newStyle.transform = `translateX(${(p - 1) * 100}%)`;
+        }
+      }
+    },
+    end(newViewEl, prevViewEl) {
+      const prevStyle = prevViewEl?.ref.style;
+      if (prevStyle) {
+        prevStyle.display = "none";
+      }
+    },
+  };
+  let closeConfig: ViewEvent = {
+    duration: 400,
+    start(closeViewEl, activeViewEl) {
+      console.log("closing");
+      const containerEl = containerRef.current;
+      if (containerEl && viewsInfo.length <= 1) {
+        containerEl.style.display = "block";
+      } else {
+        const newStyle = activeViewEl?.ref.style;
+        const prevStyle = closeViewEl?.ref.style;
+        if (newStyle) {
+          newStyle.display = "block";
+          newStyle.zIndex = "2";
+          newStyle.transform = "translateX(100%)";
+        }
+        if (prevStyle) {
+          prevStyle.zIndex = "1";
+        }
+      }
+    },
+    animate(t, closeViewEl, activeViewEl) {
+      const containerEl = containerRef.current;
+      const activeStyle = activeViewEl?.ref.style;
+      const closeStyle = closeViewEl.ref.style;
+      const p = bezier(0.25, 1, 0.5, 1)(t);
+      if (containerEl && viewsInfo.length <= 1) {
+        containerEl.style.marginLeft = `${-p * containerEl?.offsetWidth}px`;
+      } else {
+        if (activeStyle) {
+          activeStyle.transform = `translateX(${(1 - p) * 100}%)`;
+        }
+        if (closeStyle) {
+          closeStyle.transform = `translateX(${-p * 100}%)`;
+        }
+      }
+    },
+    end(closeView, activeView) {
+      const containerEl = containerRef.current;
+      const activeStyle = activeView?.ref.style;
+      const closeStyle = closeView?.ref.style;
+      if (activeStyle) {
+        activeStyle.opacity = "1";
+      }
+      if (containerEl && viewsInfo.length <= 1) {
+        containerEl.style.display = "none";
+      } else {
+        if (closeStyle) {
+          closeStyle.opacity = "0";
+          closeStyle.display = "none";
+        }
+      }
+    },
+  };
   const { viewsInfo } = useViewManage(
     containerName,
     0,
     { moveBetweenViews: true },
-    {
-      duration: 400,
-      start(newView, prevView) {
-        const newStyle = newView.ref.style;
-        const prevStyle = prevView?.ref?.style;
-        newStyle.display = "block";
-        newStyle.zIndex = "2";
-
-        if (prevStyle) {
-          prevStyle.zIndex = "1";
-        }
-      },
-      animate(t, newView, prevView) {
-        const p = slideIn(t);
-        const newStyle = newView.ref.style;
-        const prevStyle = prevView?.ref?.style;
-        newStyle.opacity = `${p}`;
-
-        if (prevStyle) {
-          prevStyle.opacity = `${1 - p}`;
-          //prevStyle.filter = `brightness(${(1 - t) * 20 + 80}%)`;
-        }
-      },
-      end(newView, prevView) {
-        const prevStyle = prevView?.ref?.style;
-        if (prevStyle) {
-          prevStyle.display = "none";
-        }
-      },
-    },
-    {
-      duration: 400,
-      start(closeView, activeView) {
-        const closeStyle = closeView.ref.style;
-        const activeStyle = activeView?.ref.style;
-        closeStyle.display = "1";
-        if (activeStyle) {
-          activeStyle.display = "block";
-          activeStyle.opacity = "1";
-        }
-      },
-      animate(t, closeView, activeView) {
-        const closeStyle = closeView.ref.style;
-        const activeStyle = activeView?.ref.style;
-
-        const p = slideIn(t);
-        closeStyle.opacity = `${1 - p}`;
-        if (activeStyle) {
-          //activeStyle.filter = `brightness(${t * 20 + 80}%)`;
-          activeStyle.opacity = `${p}`;
-        }
-      },
-      end(closeView, activeView) {
-        const closeStyle = closeView.ref.style;
-        const activeStyle = activeView?.ref.style;
-
-        closeStyle.opacity = "0";
-        closeStyle.display = "none";
-        if (activeStyle) {
-          activeStyle.opacity = "1";
-        }
-      },
-    },
-    openTabContainerConfig,
-    openTabContainerConfig,
-    onLeaveContainerConfig,
+    openConfig,
+    closeConfig,
   );
 
   return viewsInfo.length === 0 ? (
     <></>
   ) : (
-    <div className={"partial-tab-container " + className}>
+    <div ref={containerRef} className={"partial-tab-container " + className}>
       {viewsInfo?.map((viewInfo) => (
         <React.Fragment key={viewInfo.id}>
           <ViewContextProvider viewInfo={viewInfo}>
